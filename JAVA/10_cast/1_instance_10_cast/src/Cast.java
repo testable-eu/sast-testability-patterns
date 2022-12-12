d5e4de7e20a55f4ccbfd00988682e6a6cb56bc5e
pr0me <jlseidel@shiftleft.io>
2022-12-12 18:11:50 +0100

    review pattern 10 JAVA

--- JAVA/10_cast/1_instance_10_cast/src/Cast.java
+++ JAVA/10_cast/1_instance_10_cast/src/Cast.java
@@ -4,12 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-    public class Cast extends HttpServlet {
-        public void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws IOException {
-            //source
-            String s = request.getParameter("t1");
-            String s1 = String.class.cast(s) ;
-            response.getWriter().write(s1) ;
-        }
+public class Cast extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        //source
+        String s = request.getParameter("t1");
+        String s1 = String.class.cast(s) ;
+        response.getWriter().write(s1) ;
     }
+}
