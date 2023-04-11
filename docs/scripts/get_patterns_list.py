98b0217a5a3110dd507215bc6ad896244b03f945
felix-20 <felix-20@users.noreply.github.com>
2023-04-11 15:15:27 +0200

    Added version field to pattern json, adjusted script and README

--- docs/scripts/get_patterns_list.py
+++ docs/scripts/get_patterns_list.py
@@ -17,7 +17,7 @@
 
 	Location: 
 	------------
-	/testability_patterns/scripts/get_patterns_list.py
+	/testability_patterns/docs/scripts/get_patterns_list.py
 
 
 	Running:
@@ -27,13 +27,22 @@
 
 """
 
-
+import json
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
 def get_sorted_patterns(patterns):
 	return sorted(patterns, key=lambda pattern_name: int(pattern_name.split('_')[0]))
+
+
+def get_pattern_version(path_to_pattern):
+	path_to_pattern_json = os.path.join(path_to_pattern, f"{os.path.basename(path_to_pattern)}.json")
+	with open(path_to_pattern_json, "r") as json_file:
+		result = json.load(json_file)
+	return result["version"]
+
 
 def main():
 	
@@ -52,8 +61,7 @@ def main():
 	php_patterns = os.listdir(PHP_PATTERNS_DIR)
 	php_patterns = [item for item in php_patterns if os.path.isdir(os.path.join(PHP_PATTERNS_DIR, item))]
 
-
-	patterns_all = {
+	all_patterns = {
 		"JS": js_patterns,
 		"JAVA": java_patterns,
 		"PHP": php_patterns
@@ -63,107 +71,24 @@ def main():
 		"JAVA": "Java",
 		"PHP": "PHP"
 	}
-	patterns_reviewed = {
-		"JS":[
-			"13_function_declared_immediately_executed",
-			"39_function_get_arguments",
-			"1_unset_element_array",
-			"25_weak_map",
-			"32_array_shift",
-			"38_while_break",
-			"89_proxy",
-			"64_assign_object",
-			"76_reference_argument",
-			"5_variadic",
-			"28_closures",
-			"30_generator_delegation",
-			"35_async_methods",
-			"37_generators",
-			"42_anonymous_object",
-			"65_proto",
-			"67_symbol_to_string_tag",
-			"71_named_class",
-			"84_try_catch",
-			"77_object_clone",
-			"54_define_property",
-			"97_vulnerable_key_dictionary",
-			"98_throw_exception",
-			"31_generatorfunction_constructor",
-			"34_bind",
-			"55_inheritance",
-			"56_weak_ref",
-			"61_delete_properties",
-			"62_static_variable",
-			"70_reflect_get",
-			"83_getattribute",
-			"87_modules",
-			"92_set_to_array",
-			"15_reflect_delete",
-			"105_this_statement",
-			"102_event_dispatch",
-			"103_event_handler_registration",
-			"104_event_handler_removal",
-			"105_this_statement"
-		],
-		"PHP": [
-			"1_static_variables",
-			"2_global_variables",
-			"27_get_called_class",
-			"31_static_method_variable",
-			"33_get_overloading",
-			"34_isset_overloading",
-			"36_call_overloading",
-			"37_callstatic_overloading",
-			"60_array_walk",
-			"61_array_map",
-			"68_compact",
-			"69_create_function",
-			"70_extract",
-			"71_array_functions",
-			"74_dirname",
-			"75_buffe"
-		],
-		"JAVA": [
-			"1_throwable",
-			"6_callable",
-			"7_function_currying",
-			"8_tomap_2dimarr",
-			"9_beans_getvalue",
-			"10_cast",
-			"11_bytearrayinputstream",
-			"12_chararrayreader",
-			"13_runfinalization",
-			"16_type_inference",
-			"18_socketpermission",
-			"19_stringjoiner_add",
-			"21_string_valueof_override_to_string",
-			"22_addition_assignment_operator",
-			"25_assign_class_to_interface",
-			"26_assign_object1_to_object2",
-			"27_asynchronous",
-			"106_list_copyof",
-			"32_cloneable",
-			"20_inner_class_declaration",
-			"76_session",
-			"17_collections_ncopies"
-		]
-	}
 
 	with open("./patterns_status.out", 'w') as fd:
-		for language in patterns_all:
+		for language in all_patterns:
+			# Markdown for collapsible list and table headers
 			fd.write('<details markdown="1">\n')
 			fd.write(f"<summary><b>{language_lookup[language]}</b></summary>\n\n")
 			fd.write("|ID|Pattern Name|Version|Link|\n")
 			fd.write("|:--|:--|:--|:--|\n")
-			for pattern in get_sorted_patterns(patterns_all[language]):
+
+			for pattern in get_sorted_patterns(all_patterns[language]):
+				# Get details for each pattern
 				pattern_id = pattern.split("_")[0]
 				pattern_name = pattern[pattern.index(pattern_id) + len(pattern_id) + 1:]
 				pattern_link_preview = f"{language}/{pattern}"
 				pattern_link = f"{BASE_LINK_MASTER}/{pattern_link_preview}"
-				pattern_version = "v0.draft"
-				if pattern in patterns_reviewed[language]:
-					pattern_version = "v1.0"
+				pattern_version = get_pattern_version(os.path.join(BASE_DIR, language, pattern))
 				fd.write(f"|{pattern_id}|{pattern_name}|{pattern_version}|[{pattern_link_preview}]({pattern_link})|\n")
+			# Finish one collapsable list
 			fd.write("\n</details>\n\n")
 
 
